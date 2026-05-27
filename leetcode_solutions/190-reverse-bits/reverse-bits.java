41a6b3d71b8b1e0b7f8db2a6bcc450024f5a9e62
Ankit Basu <ankitbasu10@gmail.com>
2026-05-27 23:40:23 +0530

    Time: 1 ms (59.08%) | Memory: 42.6 MB (32.68%) - LeetSync

--- leetcode_solutions/190-reverse-bits/reverse-bits.java
+++ leetcode_solutions/190-reverse-bits/reverse-bits.java
@@ -1,11 +1,13 @@
-public class Solution {
+class Solution {
     public int reverseBits(int n) {
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            res <<= 1;
-            res |= (n & 1);
-            n >>>= 1;
+        int ans = 0;
+        int mask = 1;
+        for(int i=0;i<32;i++){
+            if((mask & n)!=0){
+                ans+= 1<<31-i;
+            }
+            mask<<=1;
         }
-        return res;
+        return ans;
     }
 }
