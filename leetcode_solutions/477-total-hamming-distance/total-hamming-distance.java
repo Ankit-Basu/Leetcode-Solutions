ec626505e7209a41d5e450941b8e05752a5fccd8
Ankit Basu <ankitbasu10@gmail.com>
2026-07-02 16:11:21 +0530

    Time: 15 ms (65.72%) | Memory: 48.2 MB (16.46%) - LeetSync

---
+++ leetcode_solutions/477-total-hamming-distance/total-hamming-distance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+
+        for(int bit=0; bit<32; bit++){
+            int ones = 0;
+            for(int num:nums){
+                if((num & (1<<bit))!=0) ones++;
+            }
+            int zeros = n - ones;
+            ans+= zeros * ones;
+        }
+        return ans;
+    }
+}
