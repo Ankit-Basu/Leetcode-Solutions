b7edd65befafff530aa56cb5e68e7933756a0b5c
Ankit Basu <ankitbasu10@gmail.com>
2026-09-13 03:01:10 +0530

    Time: 2 ms (99.30%) | Memory: 44.2 MB (73.24%) - LeetSync

--- leetcode_solutions/125-valid-palindrome/valid-palindrome.java
+++ leetcode_solutions/125-valid-palindrome/valid-palindrome.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
+        s = s.toLowerCase();
         int l=0;
-        int r= s.length()-1;
-        while(l<=r){
+        int r=s.length()-1;
+        while(l<r){
             char ch1 = s.charAt(l);
             char ch2 = s.charAt(r);
-
-            if(!((ch1>='a'&& ch1<='z') || (ch1>='0'&&ch1<='9'))){
+            if(!((ch1>='a'&&ch1<='z')||(ch1>='0'&&ch1<='9'))){
                 l++;
                 continue;
             }
-            if(!((ch2>='a'&& ch2<='z') || (ch2>='0'&&ch2<='9'))){
+            if(!((ch2>='a'&&ch2<='z')||(ch2>='0'&&ch2<='9'))){
                 r--;
                 continue;
             }
